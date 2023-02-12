5bc3c7d70a819ad874891b30e1154dc446ec1d9e
Fedi Nabli <fedinabli@gmail.com>
2023-02-12 21:04:32 +0100

    Added window

--- Sparky-core/main.cpp
+++ Sparky-core/main.cpp
@@ -1,13 +1,16 @@
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include "src/graphics/window.h"
 
 int main()
 {
-	if (!glfwInit())
-		std::cout << "Error" << std::endl;
-	else
-		std::cout << "Success!" << std::endl;
+	using namespace sparky;
+	using namespace graphics;
 
-	system("PAUSE");
+	Window window("Sparky!", 800, 600);
+
+	while (!window.closed())
+	{
+		window.update();
+	}
+
 	return 0;
 }
